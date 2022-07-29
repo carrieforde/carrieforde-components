165bd8df99fee416fce90fa8f179c522c97a8f6d
Carrie Forde <carrie@carrieforde.com>
2022-07-29 11:28:36 -0700

    move colors under tokens

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["react-app", "react-app/jest", "plugin:storybook/recommended"],
   rules: {
-    "no-console": ["log", { allow: ["error", "warn"] }],
+    "no-console": ["error", { allow: ["error", "warn"] }],
   },
 };
